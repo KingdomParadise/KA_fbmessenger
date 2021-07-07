43e86cd6c5c2f819943e75ef7ddc1088489217bb
mashood <mashoodurrehmanofficial@gmail.com>
2021-07-07 09:38:35 +0500

    c

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# konectai_fbmessenger
+# Mazamamedia_fbmessenger
+Facebook Automessenfer with custom chatbot API
+
+
+## Run the Project
+
+There must a server running that will provide the API service for chatbot reply.
+```
+python ./main.py
+```
+OR
+```
+py ./main.py
+```
+
+
+## Working
+Project folder consists of multiple files and each contributes to a specific functionality.
+
+### main.py
+
+This file is responsible for handling the GUI section of project and also it combines all modules together.
+
+### contact_fetch.py
+
+This file fecthes list of firends determined as maximum contacts to be fetched
+
+### auto_messenger.py
+
+This file handles the chat activity with the users and it reads data from fresh_fetched_contacts.txt to keep track of targetted users.
+
+### api.py
+
+This file is called by auto_messenger.py files and it send request to CHAT_BOT SERVER for a reply.
+
+### credentials.txt
+
+This file is used to store the recent user credentials
+
+### fresh_fetched_contacts.txt
+
+This file is used to store the ids of users determined as maximun contacts to be fetched.
+
+
+
+
---
+++ api.py
@@ -0,0 +1,45 @@
+
+import requests,json,re,emoji
+def SERVER_MESSAGE(chatid,message):
+  
+        url = "http://localhost:8000/"
+        received_messages =  requests.post(url,data={'chatid':chatid,'message':message}).json()['message']
+        if type(received_messages) is list:received_messages = " ".join(received_messages)
+        # print(received_messages)
+        def remove_emojis(data):
+            emoj = re.compile("["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                u"\U00002500-\U00002BEF"  # chinese char
+                u"\U00002702-\U000027B0"
+                u"\U00002702-\U000027B0"
+                u"\U000024C2-\U0001F251"
+                u"\U0001f926-\U0001f937"
+                u"\U00010000-\U0010ffff"
+                u"\u2640-\u2642" 
+                u"\u2600-\u2B55"
+                u"\u200d"
+                u"\u23cf"
+                u"\u23e9"
+                u"\u231a"
+                u"\ufe0f"  # dingbats
+                u"\u3030"
+                            "]+", re.UNICODE)
+            return re.sub(emoj, '', data)
+        text = remove_emojis(received_messages)
+        text = remove_emojis(received_messages).split(' ')
+        text = [x for x in text if not str(x).startswith('\\')] 
+        text = " ".join(text)
+        text = str(text).strip()
+        print(text)
+        return  text
+ 
+
+ 
+if __name__ == "__main__":
+    chatid= '0001'
+    SERVER_MESSAGE(chatid,str("mashood@gmail.com is my email 40100 hello")) 
+
+ 
